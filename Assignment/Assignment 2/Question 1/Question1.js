925540b95a1871fdffaca57689ebcc714f28cd00
BloodWolf10 <126118338+BloodWolf10@users.noreply.github.com>
2024-03-03 17:15:28 -0500

    Complete question 1

--- Assignment/Assignment 2/Question 1/Question1.js
+++ Assignment/Assignment 2/Question 1/Question1.js
@@ -1,3 +1,5 @@
+/* Json Array */
+
 let text =
   '{ "employees" : [' +
   '{ "firstName":"John" , "lastName":"Doe" },' +
@@ -6,3 +8,23 @@ let text =
 
 var obj = JSON.parse(text);
 
+
+
+  // Get the array of employees
+  let employees = obj.employees;
+
+  // Create the UL element
+  let ul = document.createElement('ul');
+
+  // Loop through the employees and create list items
+  employees.forEach(function(employee) {
+    let li = document.createElement('li');
+    li.textContent = `${employee.firstName} ${employee.lastName}`;
+    ul.appendChild(li);
+  });
+
+  // Get the div where you'll append the UL element
+  let div = document.getElementById('demo');
+
+  // Append the UL element to the div
+  div.appendChild(ul);
